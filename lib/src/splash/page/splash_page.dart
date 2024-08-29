17195c9035709125d467d910df5ea9f0ce2a243a
sangjun <byjun0302@gmail.com>
2024-08-29 12:48:57 +0900

    feat: add DataLoadController

--- lib/src/splash/page/splash_page.dart
+++ lib/src/splash/page/splash_page.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:yeezy/src/splash/controller/splash_controller.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends GetView<SplashController> {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          'Splash 페이지',
-          style: TextStyle(color: Colors.white),
-        ),
+        child: Obx(
+            () => Text(
+              '${controller.loadStep.value.name}중 입니다.',
+              style: const TextStyle(color: Colors.white),
+            )
+        )
       ),
     );
   }
