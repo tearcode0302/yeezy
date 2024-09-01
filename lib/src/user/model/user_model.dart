b42a922bfd50af4f76a16deeb8d230d71711947a
sangjun <byjun0302@gmail.com>
2024-09-01 12:41:13 +0900

    feat: make stream to authentication_repository

--- lib/src/user/model/user_model.dart
+++ lib/src/user/model/user_model.dart
@@ -1,5 +1,8 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'user_model.g.dart';
 
+@JsonSerializable()
 class UserModel extends Equatable {
   final String? uid;
   final String? name;
@@ -10,6 +13,9 @@ class UserModel extends Equatable {
     this.name,
     this.email,
   });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 
   @override
   List<Object?> get props => [
--- lib/src/user/repository/authentication_repository.dart
+++ lib/src/user/repository/authentication_repository.dart
@@ -1,7 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../model/user_model.dart';
+
 class AuthenticationRepository {
   final FirebaseAuth _firebaseAuth;
 
   AuthenticationRepository(this._firebaseAuth);
+
+  Stream<UserModel?> get user {
+    return _firebaseAuth.authStateChanges().map<UserModel?>((user) {
+      return user == null ? null : UserModel(
+        name: user.displayName,
+        uid: user.uid,
+        email: user.email,
+      );
+    });
+  }
 }
