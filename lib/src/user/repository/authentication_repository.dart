f3a37a89c457ae74fd717aa5d9e540fa9f5d5aea
sangjun <byjun0302@gmail.com>
2024-09-02 14:00:44 +0900

    feat: add google and apple login to authentication repository

--- lib/src/user/repository/authentication_repository.dart
+++ lib/src/user/repository/authentication_repository.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 import '../model/user_model.dart';
 
@@ -9,11 +15,64 @@ class AuthenticationRepository {
 
   Stream<UserModel?> get user {
     return _firebaseAuth.authStateChanges().map<UserModel?>((user) {
-      return user == null ? null : UserModel(
-        name: user.displayName,
-        uid: user.uid,
-        email: user.email,
-      );
+      return user == null
+          ? null
+          : UserModel(
+              name: user.displayName,
+              uid: user.uid,
+              email: user.email,
+            );
     });
+  }
+
+  Future<void> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn().signIn();
+    final googleAuth = await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    await _firebaseAuth.signInWithCredential(credential);
+  }
+
+  String generateNonce([int length = 32]) {
+    const charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-._';
+
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  Future<void> signInWithApple() async {
+    final rawNonce = generateNonce();
+    final nonce = sha256ofString(rawNonce);
+
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      nonce: nonce,
+    );
+
+    final oauthCredential = OAuthProvider('apple.com').credential(
+      idToken: appleCredential.identityToken,
+      rawNonce: rawNonce,
+    );
+
+    await _firebaseAuth.signInWithCredential(oauthCredential);
+  }
+
+  Future<void> logout() async {
+    await _firebaseAuth.signOut();
   }
 }
